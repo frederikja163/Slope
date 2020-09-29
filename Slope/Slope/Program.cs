514852f96e018d15c12fe7e652ee1aea484caee9
frederikja163 <fjaplv64@gmail.com>
2020-09-29 14:24:02 +0200

    initial window

---
+++ Slope/Slope/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OpenTK.Graphics.OpenGL4;
+
+namespace Slope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using Window window = new Window();
+            window.Run();
+        }
+    }
+}
---
+++ Slope/Slope/Window.cs
@@ -0,0 +1,12 @@
+﻿using OpenTK.Windowing.Desktop;
+
+namespace Slope
+{
+    public class Window : GameWindow
+    {
+        public Window()
+            : base(new GameWindowSettings(){IsMultiThreaded = true}, new NativeWindowSettings(){Title =  "Slope"})
+        {
+        }
+    }
+}
